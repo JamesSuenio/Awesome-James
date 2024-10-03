6cb7da3c69f8164c9af58de54e87ccc8bcae5339
JamesSuenio <jamesuenio09@gmail.com>
2024-10-03 21:53:56 +0800

    first commit

--- server/database.js
+++ server/database.js
@@ -3,6 +3,12 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const uri = process.env.MONGODB_URI || "mongodb://localhost:27017/";
 
+
+
+
+
+
+
 const options =  {
     serverApi: {
         version: ServerApiVersion.v1,
